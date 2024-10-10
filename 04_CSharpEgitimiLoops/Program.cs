9131fd429f363e6b4a61e5619d3c72d2dccb743f
Güven Yılmaz <23420210031@gazi.edu.tr>
2024-10-10 17:00:09 +0300

    Add new loop projects to CSharpEgitimi solution
    Two new projects, `04_CSharpEgitimiLoops` and `05_CSharpEgitimiLoopsWithStars`, were added to the `CSharpEgitimi.sln` solution. The solution configuration was updated to include build configurations for these projects.
    
    The `04_CSharpEgitimiLoops` project includes:
    - `04_CSharpEgitimiLoops.csproj`: Project properties and build configurations.
    - `App.config`: Runtime version specification.
    - `Program.cs`: Main program logic with loop and decision structure examples.
    - `Properties\AssemblyInfo.cs`: Assembly metadata.
    
    The `05_CSharpEgitimiLoopsWithStars` project includes:
    - `05_CSharpEgitimiLoopsWithStars.csproj`: Project properties and build configurations.
    - `App.config`: Runtime version specification.
    - `Program.cs`: Main program logic with examples of loops that print stars.
    - `Properties\AssemblyInfo.cs`: Assembly metadata.

---
+++ 04_CSharpEgitimiLoops/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CSharpEgitimiLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Döngüler
+            #region For Döngüsü
+            //int i;
+            //for (i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region For Döngüleri ile Karar Yapıları
+
+            //for (int i=1; i <= 12 ; i++) {
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i + " Çift Sayıdır");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i + " Tek Sayıdır");
+            //    }
+            //}
+
+            #endregion
+
+            #region ornekbakterisorusu
+            //int bacterium = 1;
+            //for (int i= 1; i <=24 ; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ". saatte bakteri sayısı: " + bacterium);
+            //}
+            #endregion
+
+            #region While Döngüsü
+            //int i = 0;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine(i+". Merhaba döngüler");
+            //    i++;
+            //}
+
+
+
+            #endregion
+
+            #region orneksınavsorusu
+            //Klavyeden girilen 3 basamaklı sayının basamakları toplamını bulan programı yazınız.
+            Console.WriteLine("3 basamaklı bir sayı giriniz: ");
+            int number = int.Parse(Console.ReadLine());
+            int ones,tens, hundreds , sum;
+
+            hundreds = number / 100;
+            tens = (number % 100) / 10;
+            ones = number % 10;
+            
+            Console.WriteLine(hundreds+ "--" +tens+ "--" +ones);
+            sum = hundreds + tens + ones;
+            Console.WriteLine("Girilen sayının basamakları toplamı: " + sum);
+
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 05_CSharpEgitimiLoopsWithStars/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_CSharpEgitimiLoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            //for(int i = 0; i <= 10; i++){
+
+            //    Console.WriteLine("*");
+            //}
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+
+            //    Console.Write("*");
+            //}
+
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
